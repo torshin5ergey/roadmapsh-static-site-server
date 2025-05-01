01d44559a6bb6d53514a95f058a2d5b06827c5af
torshin5ergey <ttorshin5ergey@gmail.com>
2025-05-01 23:14:34 +0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERVER_USER="admin"
+SERVER_IP="192.168.0.42"
+SERVER_PORT="22"
+SOURCE_DIR="./website"
+TARGET_DIR="/var/www/html/"
+RSYNC_OPTIONS="-avz --delete"
+
+# deploy
+echo "Starting deployment to $SERVER_IP..."
+rsync $RSYNC_OPTIONS $SOURCE_DIR $SERVER_USER@$SERVER_IP:$TARGET_DIR
+
+if [ $? -eq 0 ]; then
+  echo -e "\033[32m""Successfully deployed to $TARGET_DIR""\033[0m"
+else
+  echo -e "\033[31m""Deployment failed! Check errors above.""\033[0m"
+  exit 1
+fi
